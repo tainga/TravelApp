53b2c1abcb2876054fb9853e26717272fc938d74
Anastasia Taing <anastasiassd@gmail.com>
2016-05-01 18:08:28 -0400

    Add database

---
+++ src/TravelApp/Models/TripContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Data.Entity;
+
+namespace TravelApp.Models
+{
+    public class DatabaseContext : DbContext
+    {
+       
+        public DbSet<Trip> Trip { get; set; }
+        public DbSet<Stop> Stops { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = Startup.Configuration["Data:DefaultConnection:TripsConnectionString"];
+            optionsBuilder.UseSqlServer(connectionString);
+            base.OnConfiguring(optionsBuilder);
+
+        }
+        public DatabaseContext()
+        {
+            Database.EnsureCreated();
+        }
+    }
+
+
+}
--- src/TravelApp/Startup.cs
+++ src/TravelApp/Startup.cs
@@ -5,17 +5,31 @@ using System.Threading.Tasks;
 using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Hosting;
 using Microsoft.AspNet.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.PlatformAbstractions;
+using TravelApp.Models;
+using Microsoft.Data.Entity;
 
-namespace WebApplication2
+namespace TravelApp
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+
+        public static IConfigurationRoot Configuration;
+        public Startup(IApplicationEnvironment appEnv)
+        {
+            var builder = new ConfigurationBuilder()
+              .SetBasePath(appEnv.ApplicationBasePath)
+              .AddJsonFile("config.json");
+
+            Configuration = builder.Build();
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddEntityFramework().AddSqlServer().AddDbContext<TripContext>(options => options.UseSqlServer(Configuration["Data:DefaultConnection:TripsConnectionString"]));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
