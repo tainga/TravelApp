e1eea66f4ff9656268d0a8ceb6bcbb2db067b979
Anastasia Taing <anastasiassd@gmail.com>
2016-05-01 17:38:20 -0400

    Add trip and stop models

---
+++ src/TravelApp/Models/Trip.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace TravelApp.Models
+{
+    public class Trip
+    {
+        public int ID { get; set; }
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public String UserName { get; set; }
+        public ICollection<Stop> Stops { get; set; }
+    }
+}
